a7eac2530208d8dd36444043766c2f60cc3267d4
Denise Pendleton <dpendl1@hotmail.com>
2018-03-04 21:45:00 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,17 +8,17 @@ class Anagram
   end
 
   def match(possibles)
-      # 1)
-    # possibles.select {|str| word.split('').sort == str.split('').sort}
+    
+    possibles.select {|str| word.split('').sort == str.split('').sort}
     # or 2)
     # possibles.keep_if {|str| word.split('').sort == str.split('').sort}
     # or 3)
 # this also works but not if you take out the .join - if you remove.join it doesn't pass rspec
 # def match
-    @possibles = possibles
-    new_word = word.split('').sort.join
-    new_arr = possibles.collect {|x| x.split('').sort.join}
-    new_arr.select_all {|str| new_word == str}
-  end
+  #   @possibles = possibles
+  #   new_word = word.split('').sort.join
+  #   new_arr = possibles.collect {|x| x.split('').sort.join}
+  #   new_arr.select {|str| new_word == str}
+  # end
 
 end
